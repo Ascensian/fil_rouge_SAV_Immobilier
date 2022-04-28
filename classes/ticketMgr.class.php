07a17ac65f89e007a7554533c6c5d6df9dc54755
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-04-28 09:34:26 +0200

    Version 1 (ok)

--- classes/ticketMgr.class.php
+++ classes/ticketMgr.class.php
@@ -2,4 +2,13 @@
 
 class TicketMgr
 {
+    public static function getAll(string $user, string $password, int $typeEnregisterment = PDO::FETCH_ASSOC)
+    {
+        $requete = "SELECT * FROM ticketsav";
+
+        $resultat = Connexion::getConnexion($user, $password)->query($requete);
+        $tresultat = $resultat->fetchAll($typeEnregisterment);
+
+        return $tresultat;
+    }
 }
--- vues/view_ticket.php
+++ vues/view_ticket.php
@@ -1,13 +1,29 @@
 <?php
-
+spl_autoload_register(function ($classe) {
+    require "../classes/" . $classe . ".class.php";
+});
 $titre = "Ticket";
 
-ob_start();?>
+ob_start(); ?>
 
 <h1>Tickets SAV</h1>
 
+<?php $ticket = ticketMgr::getAll("root", "");
+foreach ($ticket as $key => $infoticket) { ?>
+    <div id="<?= $key ?>">
 
-<?php $contenu = ob_get_clean(); 
+        <p><a href="">Numéro de ticket : <?= $infoticket["IdTicketSAV"] ?></a></p>
+        <p>Date de création du ticket : <?= $infoticket["DateTicketSAV"] ?></p>
+        <p>Numéro de Commande : <?= $infoticket["IdCommande"] ?></p>
+        <p>Article concerné : <?= $infoticket["IdTicketSAV"] ?></p>
+        <!-- <p>Commentaire du ticket : <?= $infoticket["CommentaireTicketSAV"] ?></p> -->*
+        </a>
+    </div>
+<?php }
+?>
+
+
+<?php $contenu = ob_get_clean();
 
 
 require "../vues/gabarit.php"
