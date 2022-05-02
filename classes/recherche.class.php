976570e327eac9d42d3ab1af29624cf8d3817a79
Ascensian <my.hugogerard@protonmail.com>
2022-05-02 11:03:59 +0200

    v1 modif

---
+++ classes/recherche.class.php
@@ -0,0 +1,15 @@
+<?php
+
+class Recherche {
+    private static function rechercheClient() {
+
+    }
+
+    private static function rechercheCommande() {
+
+    }
+
+    private static function rechercheTicket () {
+        
+    }
+}
