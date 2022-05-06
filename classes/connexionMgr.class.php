b16959ebd0574c44900fb360a89e52dd7f805640
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-05-06 11:23:23 +0200

    Modification pour accès à distance

--- classes/connexionMgr.class.php
+++ classes/connexionMgr.class.php
@@ -6,7 +6,7 @@ class ConnexionMgr
     {
         $requete = "SELECT idEmploye, NomEmploye, PrenomEmploye, RoleEmploye FROM employe 
         WHERE LoginEmploye = '" . $user . "' AND MDPEmploye = '" . $password . "'";
-        $resultat = Connexion::getConnexion("root", "")->query($requete);
+        $resultat = Connexion::getConnexion("connexion", "connexion")->query($requete);
         $tresultat = $resultat->fetchAll(PDO::FETCH_ASSOC);
         if (empty($tresultat)) {
             throw new ConnexionMgrException("Erreur d'identifiant et de mot de passe");
