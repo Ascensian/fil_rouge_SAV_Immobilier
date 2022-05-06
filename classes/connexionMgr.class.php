a8a2b2eb57d5cef0d8fa1279a7cdc72c73013a9d
antoinevinciguerra <antoinevinciguerra@gmail.com>
2022-05-06 11:49:38 +0200

    Version test serveur distance

--- classes/connexionMgr.class.php
+++ classes/connexionMgr.class.php
@@ -4,9 +4,11 @@ class ConnexionMgr
 {
     public static function controleconnexion(string $user, string $password)
     {
+        $tParam = parse_ini_file("param/param.ini", true);
+        extract($tParam["BDD"]);
         $requete = "SELECT idEmploye, NomEmploye, PrenomEmploye, RoleEmploye FROM employe 
         WHERE LoginEmploye = '" . $user . "' AND MDPEmploye = '" . $password . "'";
-        $resultat = Connexion::getConnexion("connexion", "connexion")->query($requete);
+        $resultat = Connexion::getConnexion($userBDD, $mdpBDD)->query($requete);
         $tresultat = $resultat->fetchAll(PDO::FETCH_ASSOC);
         if (empty($tresultat)) {
             throw new ConnexionMgrException("Erreur d'identifiant et de mot de passe");
